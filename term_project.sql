aef281f195dc9763faf1bd5052a27ca4e3798534
William102004 <wmario2004@gmail.com>
2026-04-01 10:10:01 -0400

    database schema

---
+++ term_project.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 01, 2026 at 04:08 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `term_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Account`
+--
+
+CREATE TABLE `Account` (
+  `AccountNumber` int(11) NOT NULL,
+  `LoginName` varchar(50) NOT NULL,
+  `Balance` decimal(10,2) NOT NULL DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Budget`
+--
+
+CREATE TABLE `Budget` (
+  `BudgetID` int(11) NOT NULL,
+  `AccountNumber` int(11) NOT NULL,
+  `Category` enum('Groceries','Transportation','Entertainment','Utilities','Food') NOT NULL,
+  `Threshold` decimal(10,2) NOT NULL,
+  `Frequency` enum('Weekly','Monthly') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Transactions`
+--
+
+CREATE TABLE `Transactions` (
+  `TransactionNumber` int(11) NOT NULL,
+  `AccountNumber` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Amount` decimal(10,2) NOT NULL,
+  `Category` enum('Groceries','Transportation','Entertainment','Utilities','Food') NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `LoginName` varchar(50) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `LoginPassword` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Account`
+--
+ALTER TABLE `Account`
+  ADD PRIMARY KEY (`AccountNumber`),
+  ADD KEY `LoginName` (`LoginName`);
+
+--
+-- Indexes for table `Budget`
+--
+ALTER TABLE `Budget`
+  ADD PRIMARY KEY (`BudgetID`),
+  ADD KEY `AccountNumber` (`AccountNumber`);
+
+--
+-- Indexes for table `Transactions`
+--
+ALTER TABLE `Transactions`
+  ADD PRIMARY KEY (`TransactionNumber`),
+  ADD KEY `AccountNumber` (`AccountNumber`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`LoginName`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Account`
+--
+ALTER TABLE `Account`
+  MODIFY `AccountNumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Budget`
+--
+ALTER TABLE `Budget`
+  MODIFY `BudgetID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Transactions`
+--
+ALTER TABLE `Transactions`
+  MODIFY `TransactionNumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Account`
+--
+ALTER TABLE `Account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`LoginName`) REFERENCES `Users` (`LoginName`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Budget`
+--
+ALTER TABLE `Budget`
+  ADD CONSTRAINT `budget_ibfk_1` FOREIGN KEY (`AccountNumber`) REFERENCES `Account` (`AccountNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Transactions`
+--
+ALTER TABLE `Transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`AccountNumber`) REFERENCES `Account` (`AccountNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
